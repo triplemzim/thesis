b9caf17c951d8546a18a2abe075b80c6b5b872e8
triplemzim <triplemzim@gmail.com>
2017-09-09 10:28:10 +0600

    added static root

--- ServerFinal/settings.py
+++ ServerFinal/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 #For email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
